f79d0b70aa634a58bd52002640009cd0fe96f0ea
banteg <banteeg@gmail.com>
2021-01-15 14:20:43 +0300

    test: add tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def shared_setup(fn_isolation):
+    pass
+
+
+@pytest.fixture()
+def yfi(interface):
+    return interface.YFI("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e")
+
+
+@pytest.fixture()
+def timelock(interface, a):
+    timelock = interface.Timelock("0x026d4b8d693f6c446782c2c61ee357ec561dfb61")
+    gov = a.at(timelock.governance(), force=True)
+    return interface.Timelock(timelock, owner=gov)
+
+
+@pytest.fixture()
+def pact(YearnPact, a):
+    return YearnPact.deploy({"from": a[0]})
---
+++ tests/test_pact.py
@@ -0,0 +1,71 @@
+import pytest
+import brownie
+
+
+@pytest.mark.skip()
+def test_migration_from_timelock(yfi, timelock, pact, chain):
+    assert yfi.governance() == timelock
+    timelock.setTargetGovernance(pact)
+    # skip because of this line
+    chain.mine(timelock.period())
+    timelock.updateTargetGovernance()
+    assert yfi.governance() == pact
+    pact.sign()
+    chain.sleep(5 * 86400 * 365)
+    pact.mint()
+    assert pact.minted() == "3333 ether"
+    assert yfi.totalSupply() == "33333 ether"
+
+
+def test_migration_fake_gov(yfi, a, pact, chain):
+    gov = a.at(yfi.governance(), force=True)
+    yfi.setGovernance(pact, {"from": gov})
+    pact.sign()
+    pact.mint()
+    print("pact minted", pact.minted().to("ether"))
+    print("yfi supply", yfi.totalSupply().to("ether"))
+    assert yfi.totalSupply() > "30000 ether"
+    chain.sleep(5 * 86400 * 365)
+    pact.mint()
+    print("pact minted", pact.minted().to("ether"))
+    print("yfi supply", yfi.totalSupply().to("ether"))
+    assert pact.minted() == "3333 ether"
+    assert yfi.totalSupply() == "33333 ether"
+
+
+def test_migration_caveats(yfi, a, pact, chain):
+    gov = a.at(yfi.governance(), force=True)
+
+    # can't sign because pact is not governance
+    with brownie.reverts("dev: pact not yfi governance"):
+        pact.sign()
+
+    yfi.setGovernance(pact, {"from": gov})
+    assert yfi.governance() == pact
+    pact.sign()
+
+    # can't sign the pact twice
+    with brownie.reverts("dev: pact signed"):
+        pact.sign()
+
+    with brownie.reverts("dev: unauthorized"):
+        pact.mint({"from": a[1]})
+
+    # can mint to another account
+    mintable = pact.mintable()
+    pact.mint(a[1])
+    assert yfi.balanceOf(a[1]) == mintable
+
+    # can mint to partial amounts
+    chain.sleep(86400)
+    before = pact.minted()
+    pact.mint(a[2], "1 ether")
+    assert yfi.balanceOf(a[2]) == "1 ether"
+    assert pact.minted() - before == "1 ether"
+
+
+def test_pact_gov(a, pact):
+    pact.set_governance(a[1])
+    assert pact.pending_governance() == a[1]
+    pact.accept_governance({"from": a[1]})
+    assert pact.governance() == a[1]
